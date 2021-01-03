9ce3546fcb248459af62adca5e8905f5d9829abd
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-01-03 17:31:49 +0900

    Implement create event form

--- src/components/EventsNew.js
+++ src/components/EventsNew.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 
-// import { postEvent } from '../actions'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error },
+    } = field
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
   render() {
-    return <>hoge</>
+    const { handleSubmit, pristine, submitting } = this.props // Passed from redux-form
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div>
+          <Field label="Title" name="title" type="text" component={this.renderField} />
+        </div>
+        <div>
+          <Field label="Body" name="body" type="text" component={this.renderField} />
+        </div>
+        <div>
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    )
   }
 }
 
-// const mapDispatchToProps = { postEvent }
+const validate = values => {
+  const errors = {}
 
-export default connect(null, null)(EventsNew)
+  if (!values.title) errors.title = 'Enter a title, please.'
+  if (!values.body) errors.body = 'Enter a body, please.'
+
+  return errors
+}
+
+const mapDispatchToProps = { postEvent }
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(reduxForm({ validate, form: 'eventNewForm' })(EventsNew))
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
